e58707cf0177fcbe9e63849df56e21a168467262
afifmansib123 <78995381+afifmansib123@users.noreply.github.com>
2023-04-13 16:08:30 +0700

    Update usestate,useeffect(fetch,then)01.js

--- usestate,useeffect(fetch,then)01.js
+++ usestate,useeffect(fetch,then)01.js
@@ -2,31 +2,28 @@ import { useState,useEffect } from "react";
 
 const Myfunc = () => {
 
-  const [users, setuser] = useState([])
+  const [loading,setloading] = useState(false)
+  const [post, setpost] = useState([])
   const [error, seterror] = useState(false)
 
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/users').then((res)=>{
-      if(res.ok){
-        return res.json()
-      }else{
-        throw 'error getting user'
-      }
-    }).then((data)=>{
-      setuser(data)
-    }).catch((error)=>{
-      seterror(error)
+  const handlefetch = () => {
+    setloading(true)
+    fetch("https://jsonplaceholder.typicode.com/posts/1").then((res)=>{return res.json()})
+    .then((data)=>{
+      setpost(data)
+      setloading(false)
     })
-  },[])
-
+    .catch((err)=>{
+      seterror(true)
+      setloading(false)
+    })
+  }
   return(
-    <div>
-      {error ? <h1>error haha</h1> : 
-      <ul>
-        {users.map((user)=><li key={user.id}>{user.name}</li>)}
-      </ul>
-      }
-    </div>
+    <button onClick={handlefetch}>
+      press me
+      {loading?"wait...":""}
+      <p>{post?.title}</p>
+    </button>
   )
 }
 
